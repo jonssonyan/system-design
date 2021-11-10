3797e7a8e1341ff39cb0602d8cf63d7b686f7a36
YanZhuangZhuang <yz808@outlook.com>
2021-11-10 18:31:34 +0800

    Rate Limiter Guava

---
+++ rate-limiter/rate-limiter-guava/src/main/java/com/jonssonyan/RateLimiterGuavaApplication.java
@@ -0,0 +1,13 @@
+package com.jonssonyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RateLimiterGuavaApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(RateLimiterGuavaApplication.class, args);
+    }
+
+}
