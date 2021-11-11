2208fc60db027b4e0d6d431f4e7e351e558e85c3
YanZhuangZhuang <yz808@outlook.com>
2021-11-11 14:18:53 +0800

    Software Architecture Design

--- distributed/lock-redisson/src/main/java/com/jonssonyan/controller/TestController.java
+++ distributed/lock-redisson/src/main/java/com/jonssonyan/controller/TestController.java
@@ -1,9 +1,16 @@
 package com.jonssonyan.controller;
 
+import com.jonssonyan.entity.vo.Result;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
 @Slf4j
 public class TestController {
+
+    @GetMapping("/test")
+    public Result test() {
+        return Result.success();
+    }
 }
