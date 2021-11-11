357dea5df33b1bdcda5dce4d605d4ef99a8628c3
YanZhuangZhuang <yz808@outlook.com>
2021-11-11 17:38:35 +0800

    Lock Redisson

--- distributed/lock-redisson/src/main/java/com/jonssonyan/controller/TestController.java
+++ distributed/lock-redisson/src/main/java/com/jonssonyan/controller/TestController.java
@@ -13,6 +13,10 @@ import org.springframework.web.bind.annotation.RestController;
 
 import java.util.concurrent.TimeUnit;
 
+/**
+ * Redisson分布式锁
+ * 参考：https://github.com/redisson/redisson/wiki/8.-%E5%88%86%E5%B8%83%E5%BC%8F%E9%94%81%E5%92%8C%E5%90%8C%E6%AD%A5%E5%99%A8
+ */
 @RestController
 @Slf4j
 public class TestController {
