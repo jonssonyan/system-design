b0448b1743d9a27ed6ca915315c6be7d553339bb
jonssonyan <yz808@outlook.com>
2022-04-18 01:01:59 +0800

    Sub Table

---
+++ database/sub-table/src/main/java/com/jonssonyan/SubTableApplication.java
@@ -0,0 +1,13 @@
+package com.jonssonyan;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SubTableApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(SubTableApplication.class, args);
+    }
+
+}
