aab639fb77389374181a2b4dbc8758386eccbdef
jonssonyan <yz808@outlook.com>
2022-07-20 17:48:07 +0800

    Sharding Sub

--- database/sharding-sub/src/main/java/com/jonssonyan/controller/TOrderController.java
+++ database/sharding-sub/src/main/java/com/jonssonyan/controller/TOrderController.java
@@ -12,7 +12,7 @@ import org.springframework.web.bind.annotation.RestController;
 import java.util.List;
 
 @RestController
-@RequestMapping("/user")
+@RequestMapping("/tOrder")
 public class TOrderController {
     @Autowired
     private TOrderService tOrderService;
