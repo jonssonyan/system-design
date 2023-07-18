9b64fee892a851e62afcb958d22ef00227f3454a
jonssonyan <yz808@outlook.com>
2023-07-18 19:52:22 +0800

    SkyWalking

--- observability/skywalking/src/main/java/com/jonssonyan/SkywalkingApplication.java
+++ observability/skywalking/src/main/java/com/jonssonyan/SkywalkingApplication.java
@@ -1,13 +1,33 @@
 package com.jonssonyan;
 
+import org.apache.skywalking.apm.toolkit.trace.Trace;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 @SpringBootApplication
+@RestController
 public class SkywalkingApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(SkywalkingApplication.class, args);
     }
 
+
+    @Trace
+    @RequestMapping("test1")
+    public String test1() {
+        return "test1";
+    }
+
+    @Trace
+    @RequestMapping("test2")
+    public String test2() {
+        return test3();
+    }
+
+    private String test3() {
+        return "test3";
+    }
 }
