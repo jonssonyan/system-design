e3bdfdd29035d0152be3b6216df992b9d3d0f43b
jonssonyan <yz808@outlook.com>
2024-09-27 12:11:28 +0800

    fix

---
+++ redis/circular-queue/src/main/java/com/jonssonyan/circularqueue/CircularQueueApplication.java
@@ -0,0 +1,13 @@
+package com.jonssonyan.circularqueue;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class CircularQueueApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(CircularQueueApplication.class, args);
+	}
+
+}
---
+++ redis/circular-queue/src/test/java/com/jonssonyan/circularqueue/CircularQueueApplicationTests.java
@@ -0,0 +1,13 @@
+package com.jonssonyan.circularqueue;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CircularQueueApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
